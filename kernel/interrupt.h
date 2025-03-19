30966117718056a98f452d18bf50f7b91ee4cd64
hjh <123456@qq.com>
2025-03-20 03:16:51 +0800

    feat: implement get set view status of intr

--- kernel/interrupt.h
+++ kernel/interrupt.h
@@ -3,4 +3,18 @@
 #include "stdint.h"
 typedef void* intr_handler;
 void idt_init(void);
+
+/**
+ * 定义两种状态 0为关中断 1为开中断
+*/
+enum intr_status {
+    INTR_OFF,               // 中断关闭
+    INTR_ON                 // 中断打开
+};
+
+enum intr_status intr_get_status();
+enum intr_status intr_set_status(enum intr_status);
+enum intr_status intr_enable();
+enum intr_status intr_disable();
+
 #endif
